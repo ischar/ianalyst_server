5e3fd73c8f5019d55f79882703677e1ca3a474eb
chaflame <brownhare0221@gmail.com>
2021-08-11 08:53:13 +0900

    Add helmet

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+const helmet = require('helmet');
+
 var fs = require('fs');
+app.use(helmet());
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 app.use('/', require('./routes/main'));
-
+app.disable('x-powered-by');
 var port = 3000;
 app.listen(port, function() {
     var dir = './uploads';
